ce59974028546821c2c67f3ce932e6e82005fddf
badal kumar <bk8363385@gmail.com>
2025-11-12 09:35:07 +0000

    on monile i can se 4 products on screen without scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,7 @@ export default function Home() {
             ) : availableProducts && availableProducts.length > 0 ? (
               <section>
                 <h2 className="text-3xl font-bold text-center mb-10 font-headline">Available Now</h2>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
                   {availableProducts.map(product => (
                     <ProductCard key={product.id} product={product} />
                   ))}
