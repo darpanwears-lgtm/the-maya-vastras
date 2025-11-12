e4f4ce3a1d58472f24847b890ed10fee89dd49f1
badal kumar <bk8363385@gmail.com>
2025-11-12 07:45:11 +0000

    https://tehelka.com/media/2020/10/40-1.jpg add this as background (_for

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -35,7 +35,7 @@ export default function ProductLayout({
         )}
       >
         <Image 
-            src="https://images.unsplash.com/photo-1588523334946-f6d7c71676de?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src="https://tehelka.com/media/2020/10/40-1.jpg"
             alt="Peaceful Vedic background"
             fill
             className="object-cover w-full h-full z-0 opacity-20"
