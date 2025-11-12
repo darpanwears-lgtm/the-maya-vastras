0d5b951c0097382cf8832dbe6d866e6b92819edd
badal kumar <bk8363385@gmail.com>
2025-11-12 11:06:08 +0000

    mera mtlbheader se nahi tha screen se

--- src/components/matrix-background.tsx
+++ src/components/matrix-background.tsx
@@ -60,7 +60,7 @@ const MatrixBackground = () => {
     };
   }, []);
 
-  return <canvas ref={canvasRef} className="fixed top-0 left-0 w-full h-full z-0" />;
+  return <canvas ref={canvasRef} className="fixed top-0 left-0 w-full h-screen z-0" />;
 };
 
 export default MatrixBackground;
